7560969681236a6c6e925eaa4992752d22af0f16
ErmeHugo <sembinelli@cy-tech.fr>
2023-04-03 10:46:31 +0200

    get/set

--- src/DéposerDéchet.java
+++ src/DéposerDéchet.java
@@ -5,7 +5,62 @@ public class DéposerDéchet {
     private int qteDechet;
     private TypeDechets typeDechets;
     private int ptsgagné;
-    
 
+
+    /**
+     * @return Date return the date
+     */
+    public Date getDate() {
+        return date;
     }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    /**
+     * @return int return the qteDechet
+     */
+    public int getQteDechet() {
+        return qteDechet;
+    }
+
+    /**
+     * @param qteDechet the qteDechet to set
+     */
+    public void setQteDechet(int qteDechet) {
+        this.qteDechet = qteDechet;
+    }
+
+    /**
+     * @return TypeDechets return the typeDechets
+     */
+    public TypeDechets getTypeDechets() {
+        return typeDechets;
+    }
+
+    /**
+     * @param typeDechets the typeDechets to set
+     */
+    public void setTypeDechets(TypeDechets typeDechets) {
+        this.typeDechets = typeDechets;
+    }
+
+    /**
+     * @return int return the ptsgagné
+     */
+    public int getPtsgagné() {
+        return ptsgagné;
+    }
+
+    /**
+     * @param ptsgagné the ptsgagné to set
+     */
+    public void setPtsgagné(int ptsgagné) {
+        this.ptsgagné = ptsgagné;
+    }
+
 }
