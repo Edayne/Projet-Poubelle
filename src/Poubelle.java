e60a2d2298f59948f1b744010db6a68d5aff2eb5
Saïf-Eddine KEHILI <kehilisaif@cy-tech.fr>
2023-04-03 10:13:03 +0200

    truc

--- src/Poubelle.java
+++ src/Poubelle.java
@@ -1,9 +1,9 @@
-public class Poubelle {
+public abstract class Poubelle {
     private int idPoubelle;
     private String emplacement;
     private int capaciteMax;
     private int quantiteAct;
     private TypeDechets typeDechets;
 
-    
+    public void addUser(Utilisateur user);
 }
