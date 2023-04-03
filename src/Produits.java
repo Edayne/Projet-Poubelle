af3290004ad7aa97ea7a5c6f30baf1d179010206
ErmeHugo <sembinelli@cy-tech.fr>
2023-04-03 12:59:33 +0200

    test

--- src/Produits.java
+++ src/Produits.java
@@ -1,9 +1,47 @@
 public enum Produits {
-    Offre(string[] Alimentaire,int 15),
-    Offre2(string[] Sport,int 5),
-    Offre3(string[] HightTech, int 5),
-    Offre4(string[] Maison, int 5),
-    Offre5(string[] Jardin, int 5),
-    Offre6(string[] Vetement, int 5),
-    Offre7(string[] Animalier, int 5); 
+    Offre( 'Alimentaire', 15),
+    Offre2('Sport', 5),
+    Offre3('HightTech',  5),
+    Offre4( 'Maison',  5),
+    Offre5( 'Jardin',  5),
+    Offre6( 'Vetement',  5),
+    Offre7( 'Animalier',  5); 
+
+    private String catalog;
+    private int reduction;
+
+    
+    private Produits(String catalog,int reduction){
+        this.catalog=catalog;
+        this.reduction=reduction;
+    }
+
+    /**
+     * @return String return the catalog
+     */
+    public String getCatalog() {
+        return catalog;
+    }
+
+    /**
+     * @param catalog the catalog to set
+     */
+    public void setCatalog(String catalog) {
+        this.catalog = catalog;
+    }
+
+    /**
+     * @return int return the reduction
+     */
+    public int getReduction() {
+        return reduction;
+    }
+
+    /**
+     * @param reduction the reduction to set
+     */
+    public void setReduction(int reduction) {
+        this.reduction = reduction;
+    }
+
 }
