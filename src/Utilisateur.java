8effdf847101cb33bfc4726c2bd282b6215b45a7
Sofiane49-3 <baillemont@cy-tech.fr>
2023-04-03 10:15:01 +0200

    a

--- src/Utilisateur.java
+++ src/Utilisateur.java
@@ -3,5 +3,36 @@ public class Utilisateur {
     private String nom;
     private int codeAcces;
     private int ptsFidelite;
-    
+}
+
+public getIdUser() {
+    return idUser;
+}
+
+public void setIdUser(int idUser) {
+    this.idUser = idUser;
+}
+
+public String getNom() {
+    return nom;
+}
+
+public void setNom(String nom) {
+    this.nom = nom;
+}
+
+public int getCodeAcces() {
+    return codeAcces;
+}
+
+public void setCodeAcces(int codeAcces) {
+    this.codeAcces = codeAcces;
+}
+
+public int getPtsFidelite() {
+    return ptsFidelite;
+}
+
+public void setPtsFidelite(int ptsFidelite) {
+    this.ptsFidelite = ptsFidelite;
 }
