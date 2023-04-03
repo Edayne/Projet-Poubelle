3a25a131469438674127ddf4ea770bcb56a38c52
ErmeHugo <sembinelli@cy-tech.fr>
2023-04-03 11:59:38 +0200

    renouveler

--- src/Contrat.java
+++ src/Contrat.java
@@ -5,5 +5,63 @@ public class Contrat {
     private Date datefin;
     private Produits produitConcerné;
     private Regles demande;
-    
+
+    /**
+     * @return Date return the datedeb
+     */
+    public Date getDatedeb() {
+        return datedeb;
+    }
+
+    /**
+     * @param datedeb the datedeb to set
+     */
+    public void setDatedeb(Date datedeb) {
+        this.datedeb = datedeb;
+    }
+
+    /**
+     * @return Date return the datefin
+     */
+    public Date getDatefin() {
+        return datefin;
+    }
+
+    /**
+     * @param datefin the datefin to set
+     */
+    public void setDatefin(Date datefin) {
+        this.datefin = datefin;
+    }
+
+    /**
+     * @return Produits return the produitConcerné
+     */
+    public Produits getProduitConcerné() {
+        return produitConcerné;
+    }
+
+    /**
+     * @param produitConcerné the produitConcerné to set
+     */
+    public void setProduitConcerné(Produits produitConcerné) {
+        this.produitConcerné = produitConcerné;
+    }
+
+    /**
+     * @return Regles return the demande
+     */
+    public Regles getDemande() {
+        return demande;
+    }
+
+    /**
+     * @param demande the demande to set
+     */
+    public void setDemande(Regles demande) {
+        this.demande = demande;
+    }
+    public void renouveler(Regles demande){
+       /*     */
+    }
 }
