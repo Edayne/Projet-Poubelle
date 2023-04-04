05558603db11c00cff3d8631bef9629b43fbefa9
ErmeHugo <sembinelli@cy-tech.fr>
2023-04-04 23:16:00 +0200

    i

--- src/Contrat.java
+++ src/Contrat.java
@@ -6,62 +6,41 @@ public class Contrat {
     private Produits demande;
     private String produitConcerné=demande.getCatalog();
 
-    /**
-     * @return Date return the datedeb
-     */
+
     public Date getDatedeb() {
         return datedeb;
     }
 
-    /**
-     * @param datedeb the datedeb to set
-     */
     public void setDatedeb(Date datedeb) {
         this.datedeb = datedeb;
     }
 
-    /**
-     * @return Date return the datefin
-     */
     public Date getDatefin() {
         return datefin;
     }
 
-    /**
-     * @param datefin the datefin to set
-     */
     public void setDatefin(Date datefin) {
         this.datefin = datefin;
     }
 
-    /**
-     * @return Produits return the produitConcerné
-     */
-    public Produits getProduitConcerné() {
-        return produitConcerné;
-    }
-
-    /**
-     * @param produitConcerné the produitConcerné to set
-     */
-    public void setProduitConcerné(Produits produitConcerné) {
-        this.produitConcerné = produitConcerné;
-    }
-
-    /**
-     * @return Regles return the demande
-     */
-    public Regles getDemande() {
+    public Produits getDemande() {
         return demande;
     }
 
-    /**
-     * @param demande the demande to set
-     */
-    public void setDemande(Regles demande) {
+    public void setDemande(Produits demande) {
         this.demande = demande;
     }
-    public void renouveler(Regles demande){
-       /*     */
+
+    public String getProduitConcerné() {
+        return produitConcerné;
     }
+   
+    public void setProduitConcerné(String produitConcerné) {
+        this.produitConcerné = produitConcerné;
+    }
+
+    public void renouveler(Produits demande){
+        /*  */
+     }
+
 }
