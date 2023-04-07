2adaa30b3cefc330a5764ae4359a365951f48103
Saïf-Eddine KEHILI <kehilisaif@cy-tech.fr>
2023-04-07 16:38:19 +0200

    Les chaînes de charactères c'est avec ", pas '.

--- src/Produits.java
+++ src/Produits.java
@@ -1,12 +1,12 @@
 public enum Produits {
     /* Proposition des différentes offres */
-    Offre( 'Alimentaire', 15),
-    Offre2('Sport', 5),
-    Offre3('HightTech',  5),
-    Offre4( 'Maison',  5),
-    Offre5( 'Jardin',  5),
-    Offre6( 'Vetement',  5),
-    Offre7( 'Animalier',  5); 
+    Offre("Alimentaire", 15),
+    Offre2("Sport", 5),
+    Offre3("HightTech",  5),
+    Offre4( "Maison",  5),
+    Offre5( "Jardin",  5),
+    Offre6( "Vetement",  5),
+    Offre7( "Animalier",  5); 
 
     private String catalog;
     private int reduction;
