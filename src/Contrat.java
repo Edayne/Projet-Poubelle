9722b5d888e5e2fe66728583a2d74bdccb5442f7
Saïf-Eddine KEHILI <kehilisaif@cy-tech.fr>
2023-04-07 16:55:48 +0200

    Micro corrections des erreurs présentes

--- src/Contrat.java
+++ src/Contrat.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Contrat {
     private Date datedeb;
     private Date datefin;
-    private Produits demande;
+    private Produit demande;
     private String produitConcerné=demande.getCatalog();
 
 
@@ -37,10 +37,9 @@ public class Contrat {
     /* Et augmente la date de fin de 1 an */
  
     public void renouveler(Date datefin){
-
         Calendar calendar= Calendar.getInstance();
         calendar.setTime(datefin);
-        calendar.add(Calendar.YEAR,1);  /* Ajoute 1 an à l'instance calendar */
+        calendar.add(Calendar.YEAR, 1);  /* Ajoute 1 an à l'instance calendar */
         datefin = calendar.getTime();
         if (demande == null){
             /* On ne change pas de tarif juste la date */
@@ -51,16 +50,17 @@ public class Contrat {
             System.out.println("Veuillez saisir la rubrique du catalogue dont vous souhaitez modifier le bon d'achat : ");
             String rubrique;
             rubrique=sc.nextLine();
-            for (String name: Produits.values()){
-                name=name.getCatalog();
+
+            for (Produit produits: Produit.values()) {
+                String name=produits.getCatalog();
+                //On regarde si le produit qu'on souhaite modifier fait partie de notre catalogue
                 if (rubrique.equals(name)){
-                    pourcentage=name.getReduction();
                     Scanner sc2;
                     sc2 = new Scanner(System.in);
                     System.out.print("Saisir un pourcentage de réduction (ex: 5 pour une réduction de 5%) : ");
-                    int value;
-                    value = sc2.nextInt();
-                    pourcentage=value;
+                    int value = sc2.nextInt();
+
+                    produits.setReduction(value); //Mise à jour de la réduction offerte
                     sc2.close();
                 }
             }
--- src/Produit.java
+++ src/Produit.java
@@ -1,8 +1,8 @@
-public enum Produits {
+public enum Produit {
     /* Proposition des différentes offres */
     Offre("Alimentaire", 15),
     Offre2("Sport", 5),
-    Offre3("HightTech",  5),
+    Offre3("HighTech",  5),
     Offre4( "Maison",  5),
     Offre5( "Jardin",  5),
     Offre6( "Vetement",  5),
@@ -12,7 +12,7 @@ public enum Produits {
     private int reduction;
 
     
-    private Produits(String catalog,int reduction){
+    private Produit(String catalog,int reduction){
         this.catalog=catalog;
         this.reduction=reduction;
     }
