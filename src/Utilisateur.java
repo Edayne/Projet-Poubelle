73446da13d0f74cff1ebb3802fea6ffbbc20412b
Sofiane49-3 <baillemont@cy-tech.fr>
2023-04-10 16:11:17 +0200

    convertir points

--- src/Utilisateur.java
+++ src/Utilisateur.java
@@ -66,8 +66,8 @@ public class Utilisateur{
     Contrat contrat = commerce.getContrat();
     Produit produitConcerné = contrat.getProduitConcerné();
     int reduction = produitConcerné.getReduction();
-    int reductionAppliquee = ptsFidelite / 10 * reduction;
-    utilisateur.setPtsFidelite(ptsFidelite % 10);
+    int reductionAppliquee = ptsFidelite / 100 * reduction;
+    utilisateur.setPtsFidelite(ptsFidelite % 100);
     utilisateur.ajouterReduction(reductionAppliquee);
     System.out.println("Vous avez utilisé " + reductionAppliquee + " points de fidélité pour bénéficier d'une réduction de " + reductionAppliquee + "% sur votre prochain achat de produits " + produitConcerné.getCatalog());
 }
