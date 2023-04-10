5188b41de0db6301154bbb94d29ad9c8cd203e26
ErmeHugo <sembinelli@cy-tech.fr>
2023-04-10 11:36:41 +0200

    i

--- src/Main.java
+++ src/Main.java
@@ -9,15 +9,16 @@ public class Main {
         user1.setNom("Alice");
         user1.setCodeAcces(1234);
         user1.setPtsFidelite(50);
-        user1.setPoubelle(new Poubelle());
+        // user1.setPoubelle(new Poubelle());
         user1.setListeDeposerDechets(new ArrayList<>());
+		
 
         Utilisateur user2 = new Utilisateur();
         user2.setIdUser(2);
         user2.setNom("Bob");
         user2.setCodeAcces(5678);
         user2.setPtsFidelite(20);
-        user2.setPoubelle(new Poubelle());
+        // user2.setPoubelle(new Poubelle());
         user2.setListeDeposerDechets(new ArrayList<>());
 	}
 
