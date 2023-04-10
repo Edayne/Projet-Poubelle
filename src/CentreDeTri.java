783a61a8bb993c0a8f0a88e80361e468c99bf9d4
Sofiane49-3 <baillemont@cy-tech.fr>
2023-04-10 18:52:19 +0200

    ajout attribut stats

--- src/CentreDeTri.java
+++ src/CentreDeTri.java
@@ -5,7 +5,7 @@ public class CentreDeTri {
     private String nom;
     private String adresse;
     private ArrayList<Poubelle> listePoubelles;
-
+    private HashMap<TypeDechets, Integer> stats;
 
     
     public String getNom() {
