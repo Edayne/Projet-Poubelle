43543a6820104cc1f4ed21edef233116a1faeec2
ErmeHugo <sembinelli@cy-tech.fr>
2023-04-10 21:18:32 +0200

    i

--- src/Utilisateur.java
+++ src/Utilisateur.java
@@ -13,7 +13,7 @@ public class Utilisateur{
     private int ptsFidelite;
     private Poubelle poubelle;
     private ArrayList<DeposerDechet> listeDeposerDechets;
-    private ArrayList<Produit> listeProduits;
+    /* private ArrayList<Produit> listeProduits; */
 
     public Utilisateur(int idUser, String nom) {
         this.idUser = idUser;
