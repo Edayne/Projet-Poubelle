ef1624f6f3176f7a658ac3a863d3bcd9927d8e8a
Saïf-Eddine KEHILI <kehilisaif@cy-tech.fr>
2023-04-28 12:33:03 +0200

    Ajout de la fenetre d'accueil ( boutons centrés)

---
+++ src/FenetreAccueil.java
@@ -0,0 +1,40 @@
+
+
+import javafx.application.Application;
+import javafx.scene.*;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.geometry.Pos;
+
+public class FenetreAccueil extends Application {
+	
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("ma Fenêtre");
+		
+		Button particuliers = new Button("Particuliers");
+		Button commerce = new Button("Commerce");
+		Button centreTri = new Button("Centre de tri");
+		
+		VBox menuButtons = new VBox(5);
+		
+		StackPane root = new StackPane();
+		
+		root.getChildren().add(menuButtons);
+		menuButtons.getChildren().add(particuliers);
+		menuButtons.getChildren().add(commerce);
+		menuButtons.getChildren().add(centreTri);
+		
+		menuButtons.setAlignment(Pos.CENTER);
+		
+	    Scene scene = new Scene(root, 500, 300);
+	    primaryStage.setScene(scene);
+	    
+	    primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch (args);
+	}
+}
--- src/MyFirstAppJavaFx.java
+++ src/MyFirstAppJavaFx.java
@@ -1,40 +1,38 @@
-
-
 import javafx.application.Application;
-import javafx.scene.Scene;
+import javafx.scene.*;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class MyFirstAppJavaFx extends Application{
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
 
-@Override
-public void start(Stage primaryStage) throws Exception {
+        /* donner un nom à la fenêtre */
+        primaryStage.setTitle("ma Fenêtre");
 
-    /* donner un nom à la fenêtre */
-    primaryStage.setTitle("ma Fenêtre");
+        /* Création d'un bouton */
+        Button b = new Button("mon bouton");
 
-    /* Création d'un bouton */
-    Button b = new Button("mon bouton");
+        /* création d'une fenêtre */
+        StackPane root = new StackPane();
 
-    /* création d'une fenêtre */
-    StackPane root = new StackPane();
+        /* ajouter le bouton à la scene */
+        root.getChildren().add(b);
 
-    /* ajouter le bouton à la scene */
-    root.getChildren().add(b);
+        /* création d'une scene et de son association avec */
+        /* la fenêtre + taille */
+        Scene scene = new Scene(root, 500, 300);
 
-    /* création d'une scene et de son association avec */
-    /* la fenêtre + taille */
-    Scene scene = new Scene(root, 500, 300);
+        /* Ajouter la scene */
+        primaryStage.setScene(scene);
 
-    /* Ajouter la scene */
-    primaryStage.setScene(scene);
+        /* rendre la fenêtre visible */
+        primaryStage.show();
 
-    /* rendre la fenêtre visible */
-    primaryStage.show();
-
-    }
-    public static void main (String[] args) {
-        launch (args);
-    }
+        }
+        public static void main (String[] args) {
+            launch (args);
+        }
 }
