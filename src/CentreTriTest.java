9a3af4bf422f4bb2a78ce3bdb4f808ae37036e12
Sofiane49-3 <baillemont@cy-tech.fr>
2023-05-01 15:18:24 +0200

    a

--- src/CentreTriTest.java
+++ src/CentreTriTest.java
@@ -2,7 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import java.util.ArrayList;
 
-public class CentreDeTriTest {
+public class CentreTriTest {
 
     @Test
     public void testPlacerPoubelle() {
